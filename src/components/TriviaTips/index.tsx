51201b6736bbc08cc890a8ce05255561d0956694
Andrew Palmer <andrew.palmer.10@gmail.com>
2020-11-24 23:20:17 -0500

    Convert TriviaTips to TS

---
+++ src/components/TriviaTips/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from './styles'
+
+const TriviaTips = () => (
+  <div style={styles.tipsStyle}>
+    <div>
+      To play, say &quot;Hey Google - Answer: C&quot; or &quot;Hey Google -
+      Answer: True&quot;
+    </div>
+    <div>
+      Don&apos;t know the answer? Just guess, or say &quot;Hey Google - new
+      question&quot;
+    </div>
+  </div>
+)
+
+export default TriviaTips
